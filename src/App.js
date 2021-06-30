1f248236a1eaff5e48fceb92d0c4f38e6e8c8178
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-30 20:43:14 +0700

    header footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import React from "react";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCog, faHeart, faRedo, faStepBackward, faPause, 
+  faPlay, faStepForward, faRandom, faAd
+ } from '@fortawesome/free-solid-svg-icons'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './components/navbar/navbar'
+import Footer from './components/footer/footer'
+import Body from './components/body/body'
+
+library.add(fab, faCog, faHeart, faRedo, faStepBackward, faPause, faPlay, faStepForward, faRandom, faAd )
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar />
       </header>
-    </div>
+      <main>
+        <Body />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </Router>
   );
 }
 

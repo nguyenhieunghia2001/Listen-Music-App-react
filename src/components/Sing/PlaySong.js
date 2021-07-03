df82d5cb29bf74d5f601f18db6230657e099ceb8
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-07-03 21:46:55 +0700

    next and pre currentSong

--- src/components/Sing/PlaySong.js
+++ src/components/Sing/PlaySong.js
@@ -4,24 +4,33 @@ import TimeSlider from "react-input-slider";
 import { SingContext } from "../../context/SingContext";
 
 const Player = ({ url }) => {
-  const { currentSing, changeSing, songLikeList } = useContext(SingContext);
   const audioRef = useRef();
-  const [audioIndex, setAudioIndex] = useState(0);
+  const { currentSing, changeSing, songLikeList } = useContext(SingContext);
+  const [songsState, setSongsState] = useState([]);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const [isPlay, setPlay] = useState(currentSing.sing ? true : false);
-
   const handleLoadedData = () => {
     setDuration(audioRef.current.duration);
     if (isPlay) audioRef.current.play();
   };
 
   const toggle = () => setPlay(!isPlay);
-  // console.log('change');
+
+  //DidMount
   useEffect(() => {
-    // console.log('run');
+    // console.log('didMount Playsong');
+    const songs = localStorage.getItem("songs");
+    songs && setSongsState(JSON.parse(songs));
+  }, []);
+
+  //DidUpdate
+  useEffect(() => {
     isPlay ? audioRef.current.play() : audioRef.current.pause();
   }, [isPlay]);
+  useEffect(() => {
+    setPlay(true);
+  }, [currentSing]); // chuyển bài hát tự động play
 
   const handleTimeSliderChange = ({ x }) => {
     audioRef.current.currentTime = x;
@@ -32,14 +41,31 @@ const Player = ({ url }) => {
       audioRef.current.play();
     }
   };
-  const nextSong = () => {
-    if (currentSing) {
-      const index = songLikeList
-        .map((item) => item.id)
-        .indexOf(currentSing.sing.id);
-      const next = songLikeList[index + 1];
-      next && changeSing(next);
-    }
+  const hanleSongs = {
+    getSongWithRequest: function (control) {
+      try {
+        const index = songsState
+          .map((item) => item.id)
+          .indexOf(currentSing.sing.id);
+        const song = control === "NEXT" ? songsState[index + 1] : songsState[index - 1];
+        return song;
+      } catch (error) {
+        return undefined;
+      }
+    },
+    nextSong: function () {
+      if (currentSing) {
+        const song = hanleSongs.getSongWithRequest("NEXT");
+        song && changeSing(song);
+      }
+    },
+    preSong: function () {
+      if (currentSing) {
+        const song = hanleSongs.getSongWithRequest("PRE");
+        song && changeSing(song);
+      }
+    },
+
   };
   return (
     <div className="sing">
@@ -47,7 +73,7 @@ const Player = ({ url }) => {
         <div className="sing__control-item">
           <FontAwesomeIcon icon="redo" />
         </div>
-        <div className="sing__control-item">
+        <div className="sing__control-item" onClick={hanleSongs.preSong}>
           <FontAwesomeIcon icon="step-backward" />
         </div>
         <div className="sing__control-item" onClick={toggle}>
@@ -57,7 +83,7 @@ const Player = ({ url }) => {
             <FontAwesomeIcon icon="play" />
           )}
         </div>
-        <div className="sing__control-item">
+        <div className="sing__control-item" onClick={hanleSongs.nextSong}>
           <FontAwesomeIcon icon="step-forward" />
         </div>
         <div className="sing__control-item">
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -8,57 +8,60 @@ import axios from "axios";
 export default function (props) {
   const [singSate, setSingSate] = useState([]);
 
-  useEffect(async () =>
-    (async function () {
-      try {
-        const { data } = await axios.get(
-          "https://g0lq8.sse.codesandbox.io/sings"
-        );
-        setSingSate(data);
-      } catch (error) {
-        console.log(error);
-        return;
-      }
-    })(), []
-    // await axios
-    //     .get("https://g0lq8.sse.codesandbox.io/sings")
-    //     .then((res) => setSingSate(res.data)),
-    // []
+  //DidMount
+  // useEffect(()=> )
+
+  //DidUpdate
+  useEffect(
+    async () =>
+      (async function () {
+        try {
+          const { data } = await axios.get(
+            "https://g0lq8.sse.codesandbox.io/sings"
+          );
+          localStorage.setItem("songs", JSON.stringify(data));
+          setSingSate(data);
+        } catch (error) {
+          console.log(error);
+          return;
+        }
+      })(),
+    []
   );
-console.log('home page');
+
   return (
     // <Layout>
-      <div className="container">
-        <div className="row">
-          <div className="col-4"></div>
-          <div className="col-4" style={{ position: "fixed", zIndex: "-1" }}>
-            <div className="album__left">
-              <div className="album__img">
-                <img src={albumImg} alt="img" />
-              </div>
-              <div className="album__info">
-                <div className="album__info-title album__info-item">
-                  Mixtape Chiều Thu Họa Bóng Nàng
-                </div>
-                <div className="album__info-content album__info-item">
-                  Cập nhật: 14/05/2021
-                </div>
-                <div className="album__info-content album__info-item">
-                  DatKaa, Thiên Tú, Châu Khải Phong, ACV
-                </div>
-                <div className="album__info-content album__info-item">
-                  4k người yêu thích
-                </div>
-              </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-4"></div>
+        <div className="col-4" style={{ position: "fixed", zIndex: "-1" }}>
+          <div className="album__left">
+            <div className="album__img">
+              <img src={albumImg} alt="img" />
             </div>
-          </div>
-          <div className="col-8">
-            <div className="sings">
-              <PlayList sings={singSate} />
+            <div className="album__info">
+              <div className="album__info-title album__info-item">
+                Mixtape Chiều Thu Họa Bóng Nàng
+              </div>
+              <div className="album__info-content album__info-item">
+                Cập nhật: 14/05/2021
+              </div>
+              <div className="album__info-content album__info-item">
+                DatKaa, Thiên Tú, Châu Khải Phong, ACV
+              </div>
+              <div className="album__info-content album__info-item">
+                4k người yêu thích
+              </div>
             </div>
           </div>
         </div>
+        <div className="col-8">
+          <div className="sings">
+            <PlayList sings={singSate} />
+          </div>
+        </div>
       </div>
+    </div>
     // </Layout>
   );
 }
